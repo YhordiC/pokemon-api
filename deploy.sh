9cc544fcbb6b08d6802568ce61abd1bd97e4f4c1
Yhordi Choque Espinoza <yordychoque124@gmail.com>
2023-03-06 18:09:26 -0400

    edite el deploy

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,27 @@
-# Simple workflow for deploying static content to GitHub Pages
-name: Deploy static content to Pages
+# abort on errors
+set -e
 
-on:
-  # Runs on pushes targeting the default branch
-  push:
-    branches: ['main']
+# build
+npm run build
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+# navigate into the build output directory
+cd dist
 
-# Sets the GITHUB_TOKEN permissions to allow deployment to GitHub Pages
-permissions:
-  contents: read
-  pages: write
-  id-token: write
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
 
-# Allow one concurrent deployment
-concurrency:
-  group: 'pages'
-  cancel-in-progress: true
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
-jobs:
-  # Single deploy job since we're just deploying
-  deploy:
-    environment:
-      name: github-pages
-      url: ${{ https://github.com/YhordiC/pokemon-api }}
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v3
-      - name: Set up Node
-        uses: actions/setup-node@v3
-        with:
-          node-version: 18
-          cache: 'npm'
-      - name: Install dependencies
-        run: npm install
-      - name: Build
-        run: npm run build
-      - name: Setup Pages
-        uses: actions/configure-pages@v3
-      - name: Upload artifact
-        uses: actions/upload-pages-artifact@v1
-        with:
-          # Upload dist repository
-          path: './dist'
-      - name: Deploy to GitHub Pages
-        id: deployment
-        uses: actions/deploy-pages@v1
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+ git push -f git@github.com:YhordiC/pokemon-api.git main:gh-pages
+
+cd -
